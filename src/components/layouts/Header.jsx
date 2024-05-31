74adae3667f11454b25ac069b75985f939b010cd
Chamaththa123 <shamodchamaththa@gmail.com>
2024-05-31 15:28:10 +0530

    home page changes

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -9,7 +9,7 @@ import { HeaderLink } from "./HeaderLink";
 import { headerItems } from "../../utils/dataArrays";
 import { IoMdClose } from "react-icons/io";
 import { useStateContext } from "../../contexts/NavigationContext";
-import { UserMobileIcon ,UserIcon } from "../../utils/icons";
+import { UserMobileIcon, UserIcon } from "../../utils/icons";
 import { UserLogin } from "../../pages/user/UserLogin";
 import UserProfile from "../../pages/user/UserProfile";
 
@@ -53,9 +53,6 @@ export const Header = () => {
     };
   }, []);
 
-  const location = useLocation();
-  const currentPath = location.pathname;
-  const headerColor = currentPath === "/" ? "xl:bg-transparent fixed bg-black" : "bg-black";
 
   const [newOpen, setNewOpen] = useState(false);
   const newHandleOpen = () => setNewOpen((cur) => !cur);
@@ -79,103 +76,101 @@ export const Header = () => {
 
   return (
     <>
-    <animated.section
-      ref={ref}
-      style={fadeNavigation}
-      className={` w-full ${headerColor} inset-0 top-0 left-0 bottom-0 z-50  h-[92px]  font-inter flex items-center justify-between p-[15px] xl:py-[10px] xl:px-[40px]`}
-    >
-      <Link to="/">
-        <img src={logo} className="w-[120px] md:w-[100px]" alt="" />
-      </Link>
-      <div className="hidden xl:flex w-[70%]  justify-around font-inter">
-        {headerItems.map((item, itemindex) => {
-          return (
-            <HeaderLink url={item.url} title={item.title} key={itemindex} />
-          );
-        })}
-
-        <div className="flex justify-between mt-[-10px]">
-          <div className="text-white mr-4 mt-[3px]">
-            <UserIcon />
-          </div>
-          <div >
-            {user ? (
-              <Link onClick={handleProfileClick} to="#">
-              <div className="text-white text-[13px]">Welcome</div>
-              <div className="text-white text-[13px]">{user.firstName}</div>
-            </Link>
-            ) : (
-              <>
-                 <Link onClick={handleSignInClick} to="#">
-                    <div className="text-white text-[13px]">Welcome</div>
-                    <div className="text-white text-[13px]">SignIn / Sign up</div>
-                  </Link>
-              </>
-            )}
-          </div>
-        </div>
-      </div>
-
-      <span className="xl:hidden" onClick={handleFadeIn}>
-        <img
-          src={menu}
-          className=" w-[37px] border-4 border-[#F5F5F5] bg-[#F5F5F5] rounded-md bg-[F5F5F5]"
-          alt=""
-        />
-      </span>
-      <div
-        className={`fixed w-full inset-0 top-0 left-0 bottom-0 bg-white h-[100vh] p-[20px] transition transform duration-500 ease-in-out fade-up-enter-active ${
-          visibleMObile ? "fade-up-enter-to" : "fade-up-enter-from "
-        } `}
+      <animated.section
+        ref={ref}
+        style={fadeNavigation}
+        className={` w-full bg-black inset-0 top-0 left-0 bottom-0 z-50  h-[92px]  font-inter flex items-center justify-between p-[15px] xl:py-[10px] xl:px-[40px]`}
       >
-        <div className="flex items-center justify-between w-full">
-          <Link className="">
-            <img src={logo} className=" w-[150px]" alt="" />
-          </Link>
+        <Link to="/">
+          <img src={logo} className="w-[120px] md:w-[100px]" alt="" />
+        </Link>
+        <div className="hidden xl:flex w-[70%]  justify-around font-inter">
+          {headerItems.map((item, itemindex) => {
+            return (
+              <HeaderLink url={item.url} title={item.title} key={itemindex} />
+            );
+          })}
 
-          <span onClick={handleFadeIn}>
-            <IoMdClose className="text-[20px]" />
-          </span>
-        </div>
-        <div className="flex  mt-[30px]">
-          <div className=" mr-4 mt-[10px]">
-            <UserMobileIcon />
+          <div className="flex justify-between mt-[-10px]">
+            <div className="text-white mr-4 mt-[3px]">
+              <UserIcon />
+            </div>
+            <div>
+              {user ? (
+                <Link onClick={handleProfileClick} to="#">
+                  <div className="text-white text-[13px]">Welcome</div>
+                  <div className="text-white text-[13px]">{user.firstName}</div>
+                </Link>
+              ) : (
+                <>
+                  <Link onClick={handleSignInClick} to="#">
+                    <div className="text-white text-[13px]">Welcome</div>
+                    <div className="text-white text-[13px]">
+                      SignIn / Sign up
+                    </div>
+                  </Link>
+                </>
+              )}
+            </div>
           </div>
-          <div >
-            {user ? (
-              <Link onClick={handleProfileClick} to="#">
-              <div className=" text-[20px]">Welcome</div>
-              <div className="text-[20px]">{user?.firstName}</div>
+        </div>
+
+        <span className="xl:hidden" onClick={handleFadeIn}>
+          <img
+            src={menu}
+            className=" w-[37px] border-4 border-[#F5F5F5] bg-[#F5F5F5] rounded-md bg-[F5F5F5]"
+            alt=""
+          />
+        </span>
+        <div
+          className={`fixed w-full inset-0 top-0 left-0 bottom-0 bg-white h-[100vh] p-[20px] transition transform duration-500 ease-in-out fade-up-enter-active ${
+            visibleMObile ? "fade-up-enter-to" : "fade-up-enter-from "
+          } `}
+        >
+          <div className="flex items-center justify-between w-full">
+            <Link className="">
+              <img src={logo} className=" w-[150px]" alt="" />
             </Link>
-              // <p className="text-white">Welcome, {user.firstName}</p>
-            ) : (
-              <>
-                 <Link onClick={handleSignInClick} to="#">
+
+            <span onClick={handleFadeIn}>
+              <IoMdClose className="text-[20px]" />
+            </span>
+          </div>
+          <div className="flex  mt-[30px]">
+            <div className=" mr-4 mt-[10px]">
+              <UserMobileIcon />
+            </div>
+            <div>
+              {user ? (
+                <Link onClick={handleProfileClick} to="#">
+                  <div className=" text-[20px]">Welcome</div>
+                  <div className="text-[20px]">{user?.firstName}</div>
+                </Link>
+              ) : (
+                // <p className="text-white">Welcome, {user.firstName}</p>
+                <>
+                  <Link onClick={handleSignInClick} to="#">
                     <div className=" text-[20px]">Welcome</div>
                     <div className="text-[20px]">SignIn / Sign up</div>
                   </Link>
-              </>
-            )}
+                </>
+              )}
+            </div>
+          </div>
+          {/* mobile header  */}
+          <div className="w-full flex mt-[80px] flex-col gap-3">
+            {headerItems.map((item, itemindex) => {
+              return (
+                <div className="p-[16px] pb-[15px] font-press-start font-bold  text-[20px] leading-[24px] text-[#000000] hover:text-[#74768F] border-b-2 border-[#74768f59]">
+                  <Link to={item.url}>{item.title}</Link>
+                </div>
+              );
+            })}
           </div>
         </div>
-        {/* mobile header  */}
-        <div className="w-full flex mt-[80px] flex-col gap-3">
-          {headerItems.map((item, itemindex) => {
-            return (
-              <div className="p-[16px] pb-[15px] font-press-start font-bold  text-[20px] leading-[24px] text-[#000000] hover:text-[#74768F] border-b-2 border-[#74768f59]">
-                <Link to={item.url}>{item.title}</Link>
-              </div>
-            );
-          })}
-        </div>
-      </div>
-    </animated.section>
-    <UserLogin
-    handleOpen={newHandleOpen}
-    open={newOpen}/>
-        <UserProfile
-        handleOpen={newProfileHandleOpen}
-        open={newProfileOpen}/>
+      </animated.section>
+      <UserLogin handleOpen={newHandleOpen} open={newOpen} />
+      <UserProfile handleOpen={newProfileHandleOpen} open={newProfileOpen} />
     </>
   );
 };
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { useInView } from 'react-intersection-observer';
-import { Link } from 'react-router-dom';
-import hero from './../../assets/images/hero.mp4';
-import { Header } from '../../components/layouts/Header';
-import Footer from '../../components/layouts/Footer';
+import React, { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
+import { Link } from "react-router-dom";
+import hero from "./../../assets/images/hero.mp4";
+import { Header } from "../../components/layouts/Header";
+import Footer from "../../components/layouts/Footer";
 
 export const Home = () => {
   const [isVisible, setIsVisible] = useState(true);
@@ -22,25 +22,35 @@ export const Home = () => {
   }, [inView]);
 
   return (
-   <>
-   <Header/>
-    <section
-      ref={ref}
-      className='overflow-hidden w-full xl:h-screen md:h-[60vh]  bg-black xl:mt-0 mt-[90px] relative flex items-center justify-center font-inter'
-    >
-      <div className='absolute top-0 left-0 w-full z-50 bg-[#0c2051]'></div>
-      <video autoPlay loop muted className='w-full xl:h-screen h-[80vh] object-cover'>
-        <source src={hero} type="video/mp4" />
-      </video>
-      <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-l from-transparent to-black opacity-70"></div>
-      <div className='absolute w-full xl:h-screen h-[80vh] flex items-center flex-col justify-center gap-[30px]'>
-        <div className="text-center text-[#F2F9FF] xl:text-[90px] font-bold xl:leading-[100px] text-[45px] leading-[50px] md:mt-0 mt-4">AstroVerse</div>
-        <div className="text-center text-white xl:text-2xl font-medium  xl:leading-10 text-lg leading-[30px]"> Discover Worlds Beyond: Journey Through the Planets! </div>
-      </div>
-    </section>
-    <section className='xl:hidden block -mt-10'>
-      <Footer/>
-    </section>
-   </>
-  )
-}
+    <>
+      <Header />
+      <section
+        ref={ref}
+        className="overflow-hidden w-full xl:h-screen md:h-[60vh]  bg-black xl:mt-[-40px] mt-[90px] relative flex items-center justify-center font-inter"
+      >
+        <div className="absolute top-0 left-0 w-full z-50 bg-[#0c2051]"></div>
+        <video
+          autoPlay
+          loop
+          muted
+          className="w-full xl:h-screen h-[80vh] object-cover"
+        >
+          <source src={hero} type="video/mp4" />
+        </video>
+        <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-l from-transparent to-black opacity-70"></div>
+        <div className="absolute w-full xl:h-screen h-[80vh] flex items-center flex-col justify-center gap-[30px]">
+          <div className="text-center text-[#F2F9FF] xl:text-[90px] font-bold xl:leading-[100px] text-[45px] leading-[50px] md:mt-0 mt-4">
+            AstroVerse
+          </div>
+          <div className="text-center text-white xl:text-2xl font-medium  xl:leading-10 text-lg leading-[30px]">
+            {" "}
+            Discover Worlds Beyond: Journey Through the Planets!{" "}
+          </div>
+        </div>
+      </section>
+      <section className="md:mt-0 -mt-10">
+        <Footer />
+      </section>
+    </>
+  );
+};
